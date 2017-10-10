b0f45c8d00e2120e39fd326116825c19c2cf2c5d
jayenhc@yahoo.com <jayenhc@yahoo.com>
2017-10-10 11:26:29 +0100

    default interface

--- src/main/java/creational/singleton/EagerInitializedSingleton.java
+++ src/main/java/creational/singleton/EagerInitializedSingleton.java
@@ -14,3 +14,18 @@ public class EagerInitializedSingleton {
         return singleton;
     }
 }
+
+/*
+Sometimes in distributed systems, we need to implement Serializable
+interface in Singleton class so that we can store its state in file system
+
+The problem with serialized singleton class is that whenever we
+deserialize it, it will create a new instance of the class.
+
+So it destroys the singleton pattern, to overcome this scenario all we need to
+do it provide the implementation of readResolve() method.
+
+protected Object readResolve() {
+  return getInstance();
+}
+ */
